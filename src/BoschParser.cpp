3b5e773d0c4badba32a0fd548787ac42c8b35445
dwjoris <dwjoris@gmail.com>
2024-12-05 17:08:44 +0100

    print acc accuracy

--- src/BoschParser.cpp
+++ src/BoschParser.cpp
@@ -86,6 +86,7 @@ void BoschParser::parseMetaEvent(const struct bhy2_fifo_parse_data_info *callbac
   if (meta_event_type == BHY2_META_EVENT_SENSOR_STATUS) {
     if (byte1 == BHY2_SENSOR_ID_ACC) {
       accuracy.AccAccuracy = byte2;
+      Serial.print("Acc: "); Serial.println(byte2);
     } else if (byte1 == BHY2_SENSOR_ID_GYRO) {
       accuracy.GyroAccuracy = byte2;
     } else if (byte1 == BHY2_SENSOR_ID_MAG) {
